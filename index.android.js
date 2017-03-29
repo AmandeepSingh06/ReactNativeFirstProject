bae36ecd88647cbc70d84b5785c1a2982ee8c75f
Amandeep Singh <amandeep06singh@gmail.com>
2017-03-29 17:48:19 +0530

    random bullshit

--- index.android.js
+++ index.android.js
@@ -6,25 +6,32 @@
  import React, {Component} from 'react';
  import {AppRegistry, Text, View} from 'react-native';
 
- class Name extends Component{
 
-   render(){
-     return(
-       <Text>Hello {this.props.name} {this.props.surname}!</Text>
-     );
-   }
- }
 
  class ReactNativeFirstProject extends Component{
    render(){
      return (
        <View style={{alignItems : 'center'}}>
-        <Name name='Amandeep' surname='Singh'/>
-        <Name name='Pritesh' surname='Nandgaonkar'/>
-        <Name name='Harish' surname='Barge'/>
+       <Name name='Amandeep' surname='Singh'/>
+       <Name name='Pritesh' surname='Nandgaonkar'/>
       </View>
      );
    }
  }
+ class Name extends Component{
 
+  constructor(props){
+    super(props);
+    this.name= props.name;
+    this.surname = props.surname;
+  }
+
+  returnString = (a,b) => (a+b);
+
+  render(){
+     return(
+       <Text>Hello {this.returnString(this.name, this.surname)}!</Text>
+     );
+   }
+ }
 AppRegistry.registerComponent('ReactNativeFirstProject', () => ReactNativeFirstProject);
