69be7a8c4a611729248dddf993b20b91c2d8d2f0
Amandeep Singh <amandeep06singh@gmail.com>
2017-03-30 14:23:50 +0530

    introducing flex

--- index.android.js
+++ index.android.js
@@ -11,7 +11,7 @@
  class ReactNativeFirstProject extends Component{
    render(){
      return (
-       <View style={{alignItems : 'center'}}>
+       <View style={{flex: 1, alignItems : 'center'}}>
        <Name styleProperty={styles.red} name='Amandeep' surname='Singh'/>
        <Name styleProperty={styles.blue} name='Pritesh' surname='Nandgaonkar'/>
       </View>
@@ -26,15 +26,12 @@
     this.surname = props.surname;
     this.state = {showTest : true};
     this.styleProperty = props.styleProperty;
-    setInterval(() => {
-      this.setState({showTest: !this.state.showTest});
-    }, 1000);
   }
 
   returnString = (a,b) => (a+b);
 
   render(){
-    let display = this.state.showTest ?'Hello '+ this.returnString(this.name, this.surname) + '!' : ' ';
+    let display = 'Hello '+ this.returnString(this.name, this.surname) + '!';
      return(
        <Text style={this.styleProperty}>{display}</Text>
      );
@@ -43,12 +40,16 @@
 
  const styles = StyleSheet.create({
     red: {
-      color : 'red'
+      color : 'red',
+      backgroundColor: 'blue',
+      flex : 1,
     },
     blue: {
       color : 'blue',
+      backgroundColor: 'red',
       fontWeight : 'bold',
       fontSize : 30,
+      flex : 10,
     },
  });
 AppRegistry.registerComponent('ReactNativeFirstProject', () => ReactNativeFirstProject);
